ed46103b7b2b23adf31ec5783870a77c2ee469fe
hw3446 <hw3446@princeton.edu>
2025-04-24 12:35:11 -0400

    Rename

--- To update.R
+++ To update.R
@@ -3,11 +3,11 @@ cp -r _site/* .
 git add .
 git add -A
 git commit -m "Update site content"
-git push origin main
+git push origin main --force
 touch .nojekyll
 git add .nojekyll
 git commit -m "Add .nojekyll to prevent Jekyll processing"
-git push origin main
+git push origin main --force
 
 https://hw3446.github.io/Final_Project_PUC/
   
