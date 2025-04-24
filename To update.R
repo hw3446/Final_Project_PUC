79850315964cbefc6ef0d802716d4764b4117ba9
hw3446 <hw3446@princeton.edu>
2025-04-24 11:47:12 -0400

    Add .nojekyll to prevent Jekyll processing

--- To update.R
+++ To update.R
@@ -1,7 +1,12 @@
 quarto render
-quarto publish gh-pages
-git add _site/
-git commit -m "Update rendered site"
+cp -r _site/* .
+git add .
+git add -A
+git commit -m "Update site content"
+git push origin main
+touch .nojekyll
+git add .nojekyll
+git commit -m "Add .nojekyll to prevent Jekyll processing"
 git push origin main
 
 https://hw3446.github.io/Final_Project_PUC/
